3ce2a84b44e786e965bd56f3115cf9fe522f02ef
Thomas BRIEN <thomas.brien@centrale-marseille.fr>
2013-04-17 23:39:31 +0200

    start parsing of members

--- workspace/ObjectGrapher/src/source/Main.java
+++ workspace/ObjectGrapher/src/source/Main.java
@@ -1,11 +1,16 @@
 package source;
 
+import japa.parser.ASTHelper;
 import japa.parser.JavaParser;
 import japa.parser.ast.CompilationUnit;
+import japa.parser.ast.body.BodyDeclaration;
 import japa.parser.ast.body.MethodDeclaration;
+import japa.parser.ast.body.Parameter;
+import japa.parser.ast.body.TypeDeclaration;
 import japa.parser.ast.visitor.VoidVisitorAdapter;
 
 import java.io.FileInputStream;
+import java.util.List;
 
 public class Main {
 
@@ -24,15 +29,49 @@ public class Main {
 		} finally {
 			in.close();
 		}
+		
+		System.out.println(cu.toString());
+		String classCore = cu.toString();
+		changeMethods(cu);
+		//new MethodVisitor().visit(cu, null);
 
 		// prints the resulting compilation unit to default system output
-		System.out.println(cu.getTypes());
+		
 		System.out.println("\n\n\n\n\n\n\n");
+		//System.out.println(cu);
 		// visit and print the methods names
-		new MethodVisitor().visit(cu, null);
+		
 
 	}
 	
+	private static void changeMethods(CompilationUnit cu) {
+        List<TypeDeclaration> types = cu.getTypes();
+        for (TypeDeclaration type : types) {
+        	System.out.println("type : "+type);
+            List<BodyDeclaration> members = type.getMembers();
+            for (BodyDeclaration member : members) {
+            	System.out.println("member : "+member);
+                if (member instanceof MethodDeclaration) {
+                    MethodDeclaration method = (MethodDeclaration) member;
+                    //changeMethod(method);
+                }
+            }
+        }
+    }
+
+    private static void changeMethod(MethodDeclaration n) {
+        // change the name of the method to upper case
+        n.setName(n.getName().toUpperCase());
+
+        // create the new parameter
+        Parameter newArg = ASTHelper.createParameter(ASTHelper.INT_TYPE, "value");
+
+        // add the parameter to the method
+        ASTHelper.addParameter(n, newArg);
+    }
+
+
+        
 	/**
      * Simple visitor implementation for visiting MethodDeclaration nodes. 
      */
@@ -43,8 +82,9 @@ public class Main {
             // here you can access the attributes of the method.
             // this method will be called for all methods in this 
             // CompilationUnit, including inner class methods
-            System.out.println(n.getName());
-            System.out.println(n.getBody());
+            //System.out.println(n.getName());
+        	n.setBody(null);
+            System.out.println(n.getClass());
             System.out.println("\n");
         }
     }
